f9d893c214f1d5754bb84cceb1f13f4fe876c7a6
Noah Ratcliff <me@nratcliff.com>
2018-12-16 16:11:45 -0500

    don't pause if audio is muted

--- UnityMediaControl/MediaControl.cs
+++ UnityMediaControl/MediaControl.cs
@@ -9,23 +9,61 @@ namespace UnityMediaControl
     [InitializeOnLoad]
     internal static class MediaControl
     {
+        /// <summary>
+        /// Whether or not we recently paused playing media
+        /// </summary>
+        private static bool pausedMedia;
+
         static MediaControl()
         {
-            Debug.Log("Media Control Initialized!");
-            EditorApplication.playModeStateChanged += (state) =>
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            switch (state)
             {
-                switch (state)
-                {
-                    case PlayModeStateChange.EnteredPlayMode:
-                        // pause media
-                        User32Interop.BroadcastAppcommand(WinAppcommand.MediaPause);
-                        break;
-                    case PlayModeStateChange.ExitingPlayMode:
-                        // play media
-                        User32Interop.BroadcastAppcommand(WinAppcommand.MediaPlay);
-                        break;
-                }
-            };
+                case PlayModeStateChange.EnteredPlayMode:
+                    OnEnterPlayMode();
+                    break;
+                case PlayModeStateChange.ExitingPlayMode:
+                    OnExitPlayMode();
+                    break;
+            }
+        }
+
+        private static void OnEnterPlayMode()
+        {
+            AttemptSetMediaPlaying(false);
+        }
+
+        private static void OnExitPlayMode()
+        {
+            AttemptSetMediaPlaying(true);
+        }
+
+        private static void AttemptSetMediaPlaying(bool playing)
+        {
+
+            // do some checks to see if we need to change play state
+            if(!pausedMedia || !playing) // if we didn't already pause or are trying to pause
+            {
+                // is editor muted?
+                if (EditorUtility.audioMasterMute) return;
+            }
+
+            if (playing)
+            {
+                // play media
+                User32Interop.BroadcastAppcommand(WinAppcommand.MediaPlay);
+            }
+            else
+            {
+                // pause media
+                User32Interop.BroadcastAppcommand(WinAppcommand.MediaPause);
+            }
+
+            pausedMedia = !playing;
         }
     }
 }
