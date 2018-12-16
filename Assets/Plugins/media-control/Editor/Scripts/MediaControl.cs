192f4e48a72bd47e3bad5bfd7a0d9c61bab5691c
Noah Ratcliff <me@nratcliff.com>
2018-12-16 11:54:18 -0500

    functional

---
+++ Assets/Plugins/media-control/Editor/Scripts/MediaControl.cs
@@ -0,0 +1,29 @@
+﻿/* Noah Ratcliff - 2018 */
+
+using UnityEditor;
+using WinAppcommand = MediaControl.User32Interop.Appcommand;
+
+namespace MediaControl
+{
+    [InitializeOnLoad]
+    internal static class MediaControl
+    {
+        static MediaControl()
+        {
+            EditorApplication.playModeStateChanged += (state) =>
+            {
+                switch (state)
+                {
+                    case PlayModeStateChange.EnteredPlayMode:
+                        // pause media
+                        User32Interop.BroadcastAppcommand(WinAppcommand.MediaPause);
+                        break;
+                    case PlayModeStateChange.ExitingPlayMode:
+                        // play media
+                        User32Interop.BroadcastAppcommand(WinAppcommand.MediaPlay);
+                        break;
+                }
+            };
+        }
+    }
+}
---
+++ Assets/Plugins/media-control/Editor/Scripts/User32Interop.cs
@@ -0,0 +1,27 @@
+﻿/* Noah Ratcliff - 2018 */
+
+using System.Runtime.InteropServices;
+
+namespace MediaControl
+{
+    internal static class User32Interop
+    {
+        public enum Appcommand
+        {
+            MediaPlay = 46,
+            MediaPause = 47,
+        }
+
+        public const int HWIND_BROADCAST = 0xffff;
+
+        public const int WM_APPCOMMAND = 0x319;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, int Msg, int wParam, int lParam);
+
+        public static int BroadcastAppcommand(Appcommand command)
+        {
+            return SendMessage(HWIND_BROADCAST, WM_APPCOMMAND, 0, (int)command << 16);
+        }
+    }
+}
