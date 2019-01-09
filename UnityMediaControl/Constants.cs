dc4591348812fd0960a5b969f29ba7697ecc4e98
[name here] <[email here]>
2019-01-09 10:48:16 -0500

    add documentation

--- UnityMediaControl/Constants.cs
+++ UnityMediaControl/Constants.cs
@@ -5,16 +5,16 @@
         /// <summary>
         /// Name of the asset as listed on the Asset Store, to be used when referencing the asset inline
         /// </summary>
-        public const string AssetName = "Editor Media Manager";
+        public const string AssetName = "Editor Music Manager";
 
         /// <summary>
         /// Name of the asset to be used in the editor GUI
         /// </summary>
-        public const string GUIName = "Media Manager";
+        public const string GUIName = "Music Manager";
 
         /// <summary>
         /// The prefix attached to editor prefs stored by this asset
         /// </summary>
-        public const string EditorPrefPrefix = "EditorMediaManager_";
+        public const string EditorPrefPrefix = "EditorMusicManager_";
     }
 }
